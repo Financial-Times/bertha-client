f68df5ccd2c5e4e13741a750a45fddbf18b1ff70
Callum Locke <callum.locke@gmail.com>
2017-03-22 15:00:01 +0000

    add tests

---
+++ src/get.test.js
@@ -0,0 +1,102 @@
+/* eslint-disable no-console */
+
+import test from 'ava';
+import nock from 'nock';
+import bertha from '..';
+
+test('get single sheet', async (t) => {
+  const data = [
+    { cat: 1, dog: 2 },
+    { cat: 3, dog: 4 },
+  ];
+
+  const mock = nock('https://bertha.ig.ft.com')
+    .get('/view/publish/gss/ABC123/foo')
+    .reply(200, data)
+  ;
+
+  const response = await bertha.get('ABC123', ['foo']);
+
+  t.deepEqual(response, {
+    foo: data,
+  });
+
+  mock.done();
+});
+
+test('get multiple sheets', async (t) => {
+  const data = {
+    foo: [
+      { cat: 1, dog: 2 },
+      { cat: 3, dog: 4 },
+    ],
+    bar: [
+      { parrot: 1, fish: 2 },
+      { parrot: 3, fish: 4 },
+    ],
+  };
+
+  const mock = nock('https://bertha.ig.ft.com')
+    .get('/view/publish/gss/ABC123/bar,foo') // sheet names get sorted alphabetically
+    .reply(200, data)
+  ;
+
+  const response = await bertha.get('ABC123', ['foo', 'bar']);
+
+  t.deepEqual(response, data);
+
+  mock.done();
+});
+
+test('get single sheet with `|object` transform', async (t) => {
+  const data = [
+    { name: 'cat', value: 'yar' },
+    { name: 'dog', value: 456 },
+  ];
+
+  const mock = nock('https://bertha.ig.ft.com')
+    .get('/view/publish/gss/ABC123/foo')
+    .reply(200, data)
+  ;
+
+  const response = await bertha.get('ABC123', ['foo|object']);
+
+  t.deepEqual(response, {
+    foo: {
+      cat: 'yar',
+      dog: '456',
+    },
+  });
+
+  mock.done();
+});
+
+test('get multiple sheets with `|object` transform', async (t) => {
+  const data = {
+    foo: [
+      { name: 'cat', value: 'yar' },
+      { name: 'dog', value: 456 },
+    ],
+    bar: [
+      { parrot: 1, fish: 2 },
+      { parrot: 3, fish: 4 },
+    ],
+  };
+
+  const mock = nock('https://bertha.ig.ft.com')
+    .get('/view/publish/gss/ABC123/bar,foo')
+    .reply(200, data)
+  ;
+
+  const response = await bertha.get('ABC123', ['foo|object', 'bar']);
+
+  t.deepEqual(response, {
+    foo: {
+      cat: 'yar',
+      dog: '456',
+    },
+    bar: data.bar,
+  });
+
+  mock.done();
+});
