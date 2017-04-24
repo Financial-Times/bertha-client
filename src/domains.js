1089a9ed586d44c224b9255baa9d92ec77d9ec08
Callum Locke <callum.locke@gmail.com>
2017-04-24 15:40:32 +0100

    add parseKey and domains

---
+++ src/domains.js
@@ -0,0 +1,8 @@
+const domains = Object.freeze([
+  'bertha.ig.ft.com',
+  'spottiswood.herokuapp.com',
+  'staging.bertha.ig.ft.com',
+  'spottiswood-tupp.herokuapp.com',
+]);
+
+export default domains;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // @flow
 
 import get from './get';
+import domains from './domains';
+import parseKey from './parseKey';
 
 export {
   get,
+  domains,
+  parseKey,
 };
